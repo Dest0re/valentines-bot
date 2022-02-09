7322f2abdde848198fa01d7e685e2a95987bcce6
Dest0re <klacik2002@yandex.ru>
2022-02-09 13:51:12 +0500

    Add simple database test

--- valentinesbot/main.py
+++ valentinesbot/main.py
@@ -1,9 +1,14 @@
 from discordclient import bot
 
 from utils import EnvironmentVariables
+from model import Presenter, Receiver, ValentineCard
 
 env = EnvironmentVariables('DISCORD_BOT_TOKEN')
 
 
 if __name__ == '__main__':
+    print(tuple(Presenter.select().execute()))
+    print(tuple(Receiver.select().execute()))
+    print(tuple(ValentineCard.select().execute()))
+
     bot.run(env.DISCORD_BOT_TOKEN)
