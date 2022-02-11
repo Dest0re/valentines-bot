8e1aa0b4ed016b242a83e8dc975a8e713ef264b7
Dest0re <klacik2002@yandex.ru>
2022-02-11 16:00:47 +0500

    Add working RegisterSimpleUserHandler

--- valentinesbot/discordclient/handlers/registersimpleuser.py
+++ valentinesbot/discordclient/handlers/registersimpleuser.py
@@ -2,8 +2,10 @@ import discord
 
 from utils.embed import DebugText
 from .basehandler import BaseHandler
+from .abstractbasehandler import StopHandleException
+from model import User
 
 
 class RegisterSimpleUserHandler(BaseHandler):
     async def _handle(self, ctx: discord.ApplicationContext):
-        await ctx.respond(embed=DebugText(str(__class__)))
+        user = User.get_or_create(discord_user_id=ctx.author.id)[0]
