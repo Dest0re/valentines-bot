67fe525869b5b064bd0c0459429b420644d315f4
Dest0re <klacik2002@yandex.ru>
2022-02-11 16:01:01 +0500

    Add working CheckIfCanGiftSpecialCardHandler

--- valentinesbot/discordclient/handlers/checkifcangiftspecialcard.py
+++ valentinesbot/discordclient/handlers/checkifcangiftspecialcard.py
@@ -2,8 +2,26 @@ import discord
 
 from utils.embed import DebugText
 from .basehandler import BaseHandler
+from model import User, Presenter, ValentineCard
 
 
 class CheckIfCanGiftSpecialCardHandler(BaseHandler):
     async def _handle(self, ctx: discord.ApplicationContext):
-        await ctx.respond(embed=DebugText(str(__class__)))
+        special_cards = (
+            ValentineCard
+            .select()
+            .join(Presenter)
+            .join(User)
+            .where(
+                ValentineCard.is_special, 
+                ValentineCard.do_present, 
+                not ValentineCard.in_process, 
+                User.discord_user_id == ctx.author.id
+            )
+            .execute()
+        )
+        
+        if len(special_cards) < 1:
+            await ctx.respond(embed=DebugText('Остались специальные валентинки'))
+        else:
+            await ctx.respond(embed=DebugText('специальных валентинок не осталось'))
