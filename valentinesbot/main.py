7b3d4e55522cc9c1449ef6395d39e4d12d345e55
Dest0re <klacik2002@yandex.ru>
2022-02-11 16:00:00 +0500

    Remove model tests from main.py

--- valentinesbot/main.py
+++ valentinesbot/main.py
@@ -7,8 +7,4 @@ env = EnvironmentVariables('DISCORD_BOT_TOKEN')
 
 
 if __name__ == '__main__':
-    print(tuple(Presenter.select().execute()))
-    print(tuple(Receiver.select().execute()))
-    print(tuple(ValentineCard.select().execute()))
-
     bot.run(env.DISCORD_BOT_TOKEN)
