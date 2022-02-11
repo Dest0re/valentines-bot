3abd88ac467d8d03b34d6a5aebcba374b4bdff53
Dest0re <klacik2002@yandex.ru>
2022-02-11 16:00:30 +0500

    Add working RegisterSimpleCardHandler

--- valentinesbot/discordclient/handlers/registersimplecard.py
+++ valentinesbot/discordclient/handlers/registersimplecard.py
@@ -1,9 +1,18 @@
 import discord
 
-from utils.embed import DebugText
+from utils.embed import DebugText, ErrorText
 from .basehandler import BaseHandler
+
+from model import ValentineCard, Presenter, User
 
 
 class RegisterSimpleCardHandler(BaseHandler):
     async def _handle(self, ctx: discord.ApplicationContext):
-        await ctx.respond(embed=DebugText(str(__class__)))
+        user = User.get_or_none(discord_user_id=ctx.author.id)
+
+        if not user:
+            await ctx.respond(embed=ErrorText('Что-то пошло не так'))
+
+        presenter = Presenter.get_or_create(user=user)[0]
+
+        ValentineCard.create(presenter=presenter, in_process=True)
