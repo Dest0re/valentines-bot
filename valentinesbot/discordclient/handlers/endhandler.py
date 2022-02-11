fc201a4f8f14787563e59f93ca284d80d0965323
Dest0re <klacik2002@yandex.ru>
2022-02-11 15:59:37 +0500

    Add working EndHandler

--- valentinesbot/discordclient/handlers/endhandler.py
+++ valentinesbot/discordclient/handlers/endhandler.py
@@ -1,9 +1,19 @@
 import discord
 
+from model import ValentineCard, Presenter, User
 from utils.embed import DebugText
 from .basehandler import BaseHandler
 
 
 class EndHandler(BaseHandler):
     async def _handle(self, ctx: discord.ApplicationContext):
-        await ctx.respond(embed=DebugText(str(__class__)))
+        user = User.get_or_none(discord_user_id=ctx.author.id)
+
+        if user:
+            presenter = Presenter.get_or_create(user=user)[0]
+
+            card = ValentineCard.get_last_presenter_card_or_none(presenter)
+
+            if card:
+                card.in_process = False
+                card.save()
