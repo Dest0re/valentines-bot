f28d492e6a72b83a3c9a4cc33c8df38544e50bba
Dest0re <klacik2002@yandex.ru>
2022-02-12 15:14:51 +0500

    Add working MarkCardAsSpecialHandler

--- valentinesbot/discordclient/handlers/markcardasspecial.py
+++ valentinesbot/discordclient/handlers/markcardasspecial.py
@@ -1,9 +1,25 @@
 import discord
 
 from utils.embed import DebugText
+from .abstractbasehandler import StopHandleException
 from .basehandler import BaseHandler
+from model import ValentineCard, User, Presenter
 
 
 class MarkCardAsSpecialHandler(BaseHandler):
     async def _handle(self, ctx: discord.ApplicationContext):
-        await ctx.respond(embed=DebugText(str(__class__)))
+        user = User.get_or_none(discord_user_id=ctx.author.id)
+
+        if not user:
+            raise StopHandleException(str(__class__))
+
+        presenter = Presenter.get_or_create(user=user)[0]
+
+        card = ValentineCard.get_last_presenter_card_or_none(presenter)
+
+        if not card:
+            raise StopHandleException(str(__class__))
+
+        card.is_special = True
+
+        card.save()
