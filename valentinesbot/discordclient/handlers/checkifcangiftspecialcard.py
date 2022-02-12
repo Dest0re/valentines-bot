631a1af5cf1a066a4e9f83cfc2991df7f801773f
Dest0re <klacik2002@yandex.ru>
2022-02-13 00:35:20 +0500

    Fix algorithm

--- valentinesbot/discordclient/handlers/checkifcangiftspecialcard.py
+++ valentinesbot/discordclient/handlers/checkifcangiftspecialcard.py
@@ -8,19 +8,19 @@ from utils.strings import text_strings as ts
 
 class CheckIfCanGiftSpecialCardHandler(BaseHandler):
     async def _handle(self, ctx: discord.ApplicationContext):
+        presenter = Presenter.from_discord_id(ctx.author.id)
+
         special_cards = (
             ValentineCard
             .select()
-            .join(Presenter)
-            .join(User)
             .where(
                 ValentineCard.is_special == True,
                 ValentineCard.do_present == True,
-                not ValentineCard.in_process == False,
-                User.discord_user_id == ctx.author.id
+                ValentineCard.in_process == False,
+                ValentineCard.presenter == presenter
             )
             .execute()
         )
         
-        if len(special_cards) < 1:
+        if len(tuple(special_cards)) < 1:
             await ctx.respond(embed=EmbedText(ts.can_give_special_card))
