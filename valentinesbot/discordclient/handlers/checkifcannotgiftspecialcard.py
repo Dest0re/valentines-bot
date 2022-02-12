f9fe797d065174a2ee9ec5050324a8a8cab37f2d
Dest0re <klacik2002@yandex.ru>
2022-02-12 15:12:21 +0500

    Add working checkifcannotgiftspecialcard.py

--- valentinesbot/discordclient/handlers/checkifcannotgiftspecialcard.py
+++ valentinesbot/discordclient/handlers/checkifcannotgiftspecialcard.py
@@ -1,9 +1,33 @@
 import discord
 
-from utils.embed import DebugText
+from utils.embed import DebugText, WarningText, ErrorText
+from .abstractbasehandler import StopHandleException
 from .basehandler import BaseHandler
+from model import ValentineCard, Presenter, User
+from utils.strings import text_strings as ts
 
 
 class CheckIfCannotGiftSpecialCardHandler(BaseHandler):
     async def _handle(self, ctx: discord.ApplicationContext):
-        await ctx.respond(embed=DebugText(str(__class__)))
+        user = User.get_or_none(discord_user_id=ctx.author.id)
+
+        if not user:
+            raise StopHandleException(str(__class__))
+
+        presenter = Presenter.get_or_create(user=user)[0]
+
+        cards = (
+            ValentineCard
+            .select()
+            .where(
+                ValentineCard.presenter == presenter,
+                ValentineCard.is_special == True,
+                ValentineCard.in_process == False,
+                ValentineCard.do_present == True
+            )
+            .execute()
+        )
+
+        if len(tuple(cards)) > 0:
+            await ctx.respond(embed=ErrorText(ts.cannot_give_special_card))
+            raise StopHandleException(str(__class__))
