9a5ba806331a72127f30ca10a9a162db37da08d1
Dest0re <klacik2002@yandex.ru>
2022-02-12 15:15:49 +0500

    Add working AskForAnonymityHandler

--- valentinesbot/discordclient/handlers/askforanonymity.py
+++ valentinesbot/discordclient/handlers/askforanonymity.py
@@ -1,9 +1,53 @@
-import discord
+import asyncio
 
-from utils.embed import DebugText
+import discord
+from discord.ui.select import SelectOption
+
+from utils.embed import DebugText, EmbedText, ErrorText, SuccessText
+from .abstractbasehandler import StopHandleException
 from .basehandler import BaseHandler
+from .view.dropdown import PersonalOneChoiceDropdown
+from model import User, ValentineCard, Presenter
+from utils.strings import text_strings as ts
 
 
 class AskForAnonymityHandler(BaseHandler):
     async def _handle(self, ctx: discord.ApplicationContext):
-        await ctx.respond(embed=DebugText(str(__class__)))
+        presenter = Presenter.from_discord_id(ctx.author.id)
+
+        card = ValentineCard.get_last_presenter_card_or_none(presenter)
+        normal_emoji = ctx.bot.get_emoji(941994361426690048)
+        anonymous_emoji = ctx.bot.get_emoji(941993632041406466)
+
+        if not card:
+            raise KeyError()
+
+        dropdown = PersonalOneChoiceDropdown(
+            ctx.author,
+            ts.anonymity_choice_placeholder,
+            options=[
+                SelectOption(label="Не анонимно", emoji=normal_emoji),
+                SelectOption(label="Анонимно", emoji=anonymous_emoji),
+            ]
+        )
+
+        message = await ctx.respond(embed=EmbedText(ts.ask_for_anonymity), view=discord.ui.View(dropdown))
+
+        try:
+            choice = await dropdown.get_choices(300)
+        except asyncio.TimeoutError:
+            await ctx.respond(embed=ErrorText(ts.timeout_error))
+            raise StopHandleException(str(__class__))
+
+        new_text = "Вы выбрали: " + choice.label
+
+        dropdown.disabled = True
+        await message.edit(embed=SuccessText(new_text), view=discord.ui.View(dropdown))
+
+        match choice.label:
+            case "Не анонимно":
+                card.is_anonymous = False
+            case "Анонимно":
+                card.is_anonymous = True
+
+        card.save()
